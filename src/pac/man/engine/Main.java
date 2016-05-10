86f5c48874751c303ac3d2875a2ed0fb5195f4ee
Aaron Roy <Aaron Roy>
2016-05-10 11:22:44 -0400

    Initial Push

---
+++ src/pac/man/engine/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Main.java		May 10, 2016, 10:59:34 AM
+ */
+package pac.man.engine;
+
+/**
+ * Initializes stuff
+ * 
+ * @author Aaron Roy
+ * @version 0.0.1.0
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
